1c7f6fcd5b7a33065911dd2ca2d850ab8dcddb13
Matt Macdonald <mksmacdonald@gmail.com>
2021-07-25 12:47:18 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const ENV = process.env;
+const PORT = ENV.PORT || 8001;
+
+const app = require("./application")(ENV, { updateAppointment });
+const server = require("http").Server(app);
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  socket.onmessage = (event) => {
+    console.log(`Message Received: ${event.data}`);
+
+    if (event.data === "ping") {
+      socket.send(JSON.stringify("pong"));
+    }
+  };
+});
+
+function updateAppointment(id, interview) {
+  wss.clients.forEach(function eachClient(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({
+          type: "SET_INTERVIEW",
+          id,
+          interview,
+        })
+      );
+    }
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} in MATT mode.`);
+});
